e0ebd7d2482b9d6b7781b70863ab5616d10aa0f2
Derek Brito <derekbritoaraujo@gmail.com>
2026-04-01 17:02:37 -0600

    backend para la pantalla de carga listo

--- frontend/animacionCarga/carga.js
+++ frontend/animacionCarga/carga.js
@@ -1,4 +1,1 @@
-// LÓGICA DE LA ANIMACIÓN DE CARGA.
-// - (Opcional) Si la animación de los vinilos girando no se hace 100% con CSS, aquí va la lógica matemática de rotación.
-// - También puede servir para cambiar el texto de carga dinámicamente (ej. pasar de "Leyendo archivos..." a "Calculando tops...").
-// - etc.
+// de momento no hagan nada aqui
--- visualizaciones/pantallaCarga.py
+++ visualizaciones/pantallaCarga.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import time
+import random
 
 def mostrar_pantalla_carga():
+    if "analisis_listo" not in st.session_state:
+        st.session_state["analisis_listo"] = False
+    if "animacion_elegida" not in st.session_state:
+        st.session_state["animacion_elegida"] = random.randint(1,5)
+    
+    numero = st.session_state["animacion_elegida"]
     rutaCssCarga = "frontend/animacionCarga/carga.css"
     rutaHtmlCarga = "frontend/animacionCarga/carga.html"
     rutaJsCarga = "frontend/animacionCarga/carga.js"
@@ -11,28 +18,42 @@ def mostrar_pantalla_carga():
             st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
     except FileNotFoundError:
         pass
-    try:
-        with open(rutaCssCarga, "r",encoding="utf-8") as f:
+    try: 
+        with open(rutaCssCarga, "r", encoding="utf-8") as f:
             codigoCss = f.read()
         with open(rutaHtmlCarga, "r", encoding="utf-8") as f:
-            codigoHtml = f.read()
+            htmlCrudo = f.read()
         with open(rutaJsCarga, "r", encoding="utf-8") as f:
             codigoJs = f.read()
+        
+        htmlListo = htmlCrudo.replace("NUMERO", str(numero))
+
         paqueteCompleto = f"""
         <style>{codigoCss}</style>
-        {codigoHtml}
-        <script>{codigoJs}</script>
-        """
-        st.markdown(paqueteCompleto,unsafe_allow_html=True)
+        {htmlListo}
+        <script>{codigoJs}</script>"""
+        st.markdown(paqueteCompleto, unsafe_allow_html=True)
     except:
-        st.warning(f"Esperando el archivo frontend")
-    if st.session_state["motor"] == "motorjson":
-        time.sleep(10)
-        st.session_state["pantalla_actual"] = "dashboardjson"
-        st.write("dashboard json")
-        st.rerun()
-    elif st.session_state["motor"] == "motoroauth":
-        time.sleep(10)
-        st.session_state["pantalla_actual"] = "dashboardoauth"
-        st.write("dashboard oauth")
-        st.rerun()
+        st.warning("esperando archivos frontend")
+
+    if not st.session_state["analisis_listo"]:
+        if st.session_state["motor"] == "motorjson":
+            time.sleep(10)
+            st.session_state["analisis_listo"] = True
+            st.rerun()
+        elif st.session_state["motor"] == "motoroauth":
+            time.sleep(10)
+            st.session_state["analisis_listo"] = True
+            st.rerun()
+    
+    if st.session_state["analisis_listo"]:
+        st.success("Pibble terminó el análisis")
+        if st.button("Ver resultados"):
+            if st.session_state["motor"] == "motorjson":
+                st.session_state["pantalla_actual"] = "dashboardjson"
+            elif st.session_state["motor"] == "motoroauth":
+                st.session_state["pantalla_actual"] = "dashboardoauth"
+            
+            st.session_state["analisis_listo"] = False
+            del st.session_state["animacion_elegida"]
+            st.rerun()
