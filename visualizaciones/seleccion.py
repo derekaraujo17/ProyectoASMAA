69421ed67c786edd33037e41e7b73f5dd930341f
Derek Brito <derekbritoaraujo@gmail.com>
2026-04-07 13:51:21 -0600

    mensaje de error y deshabilitación del botón cuando se escoge una opción

--- visualizaciones/seleccion.py
+++ visualizaciones/seleccion.py
@@ -1,5 +1,12 @@
 import streamlit as st
 from visualizaciones.header import render_header
+
+def cargarArchivos(archivosSubidos):
+            archivosValidos=[]
+            if len(archivosSubidos)>= 1:
+                archivosValidos = [archivo for archivo in archivosSubidos if "StreamingHistory_music" in archivo.name]
+            return archivosValidos
+        
 def mostrar_pantalla_botones():
     #conexión con css
     rutaCssGlobal="frontend/estilosGlobales.css"
@@ -23,25 +30,20 @@ def mostrar_pantalla_botones():
         st.markdown("<h3 class='titulo-columna'>Analizar historial json</h3>",unsafe_allow_html=True)
         st.markdown("<img src='fotobotonjson' class='foto-seleccion-json'>",unsafe_allow_html=True)
         archivosSubidos = st.file_uploader("Sube tus archivos JSON", accept_multiple_files=True, type=["json"])
-        def cargarArchivos():
-            archivosValidos=[]
-            if len(archivosSubidos)>= 1:
-                archivosValidos = [archivo for archivo in archivosSubidos if "StreamingHistory_music" in archivo.name]
-                if len(archivosValidos)==0:
-                    st.write("""Error al subir los archivos""")
-            return archivosValidos
-        jsonValidos = cargarArchivos()
+        jsonValidos = cargarArchivos(archivosSubidos)
         if len(jsonValidos)>=1:
             if st.button("Analizar mis archivos"):
                 st.session_state["jsonValidos"] = jsonValidos
                 st.session_state["motor"] = "motorjson"
                 st.session_state["pantalla_actual"] = "pantallaCarga"
                 st.rerun()
+        elif (len(archivosSubidos)>0):
+             st.error("Por favor, sube los archivos de 'StreamingHistory_music' que descargaste de spotify")
     with col2:
         #botón oauth
         st.markdown("<h3 class='titulo-columna'>Conectar con spotify</h3>",unsafe_allow_html=True)
         st.markdown("<img src='fotobotonoauth' class='foto-seleccion-oauth'>",unsafe_allow_html=True)
-        if st.button("Datos actuales"):
+        if st.button("Datos actuales", disabled=len(jsonValidos)>0):
             st.session_state["motor"] = "motoroauth"
             st.session_state["pantalla_actual"] = "pantallaCarga"
             st.rerun()
