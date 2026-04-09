046548758e0d55cf33ad33cfca65ff5735b7d453
Derek Brito <derekbritoaraujo@gmail.com>
2026-04-09 12:25:56 -0600

    cambio en la lógica para la lectura de los gifs

--- visualizaciones/pantallaCarga.py
+++ visualizaciones/pantallaCarga.py
@@ -1,18 +1,36 @@
 import streamlit as st
 import time
 import random
+import base64
+import os
 
 @st.cache_data(show_spinner=False)
 def ejecutar_motor_json(archivos):
     from logica.motorjson import procesarDatosJson
     return procesarDatosJson(archivos)
 
+def obtener_gif_base64(numero):
+    rutasGifs = {
+        1: "frontend/animacionCarga/links/intro_KeyboardCat.gif",
+        2: "frontend/animacionCarga/links/intro_KeyboardCat.gif",
+        3: "frontend/animacionCarga/links/intro_KeyboardCat.gif",
+        4: "frontend/animacionCarga/links/intro_KeyboardCat.gif",
+        5: "frontend/animacionCarga/links/intro_KeyboardCat.gif"
+    }
+    ruta = rutasGifs.get(numero, rutasGifs[1])
+    try:
+        with open(ruta, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode()
+        return f"data:image/gif;base64, {encoded_string}"
+    except FileNotFoundError:
+        return ""
 def mostrar_pantalla_carga():
     if "analisis_listo" not in st.session_state:
         st.session_state["analisis_listo"] = False
     if "animacion_elegida" not in st.session_state:
         st.session_state["animacion_elegida"] = random.randint(1,5)
-    
+    if "ui_renderizada" not in st.session_state:
+        st.session_state["ui_renderizada"]=False
     numero = st.session_state["animacion_elegida"]
     rutaCssCarga = "frontend/animacionCarga/carga.css"
     rutaHtmlCarga = "frontend/animacionCarga/carga.html"
@@ -32,6 +50,8 @@ def mostrar_pantalla_carga():
         with open(rutaJsCarga, "r", encoding="utf-8") as f:
             codigoJs = f.read()
         htmlListo = htmlCrudo.replace("NUMERO", str(numero))
+        gif_base64 = obtener_gif_base64(numero)
+        htmlListo = htmlCrudo.replace("{{GIF_BASE64}}",gif_base64)
         paqueteCompleto = f"""
         <style>{codigoCss}</style>
         {htmlListo}
@@ -41,6 +61,10 @@ def mostrar_pantalla_carga():
     except:
         st.warning("esperando archivos frontend")
 
+    if not st.session_state["ui_renderizada"]:
+        st.session_state["ui_renderizada"] = True
+        time.sleep(0.5)
+        st.rerun()
     if not st.session_state["analisis_listo"]:
         with st.spinner("Pibble está analizando tus datos, esto puede tardar unos segundos..."):
             if st.session_state["motor"] == "motorjson":
@@ -67,4 +91,5 @@ def mostrar_pantalla_carga():
             
             st.session_state["analisis_listo"] = False
             del st.session_state["animacion_elegida"]
+            del st.session_state["ui_renderizada"]
             st.rerun()
