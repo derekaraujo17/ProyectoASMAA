e3caf970dcb0cd557e240b425bfb7b28a015a7c4
Derek Brito <derekbritoaraujo@gmail.com>
2026-04-09 11:29:31 -0600

    arreglo preliminar del error en el que el usuario puede eliminar sus archivos durante el análisis

--- visualizaciones/pantallaCarga.py
+++ visualizaciones/pantallaCarga.py
@@ -23,6 +23,7 @@ def mostrar_pantalla_carga():
             st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
     except FileNotFoundError:
         pass
+    contenedorAnimacion = st.empty()
     try: 
         with open(rutaCssCarga, "r", encoding="utf-8") as f:
             codigoCss = f.read()
@@ -30,32 +31,33 @@ def mostrar_pantalla_carga():
             htmlCrudo = f.read()
         with open(rutaJsCarga, "r", encoding="utf-8") as f:
             codigoJs = f.read()
-        
         htmlListo = htmlCrudo.replace("NUMERO", str(numero))
-
         paqueteCompleto = f"""
         <style>{codigoCss}</style>
         {htmlListo}
         <script>{codigoJs}</script>"""
-        st.markdown(paqueteCompleto, unsafe_allow_html=True)
+        contenedorAnimacion.markdown(paqueteCompleto, unsafe_allow_html=True)
+        time.sleep(0.2)
     except:
         st.warning("esperando archivos frontend")
 
     if not st.session_state["analisis_listo"]:
-        if st.session_state["motor"] == "motorjson":
-            try:
-                resultados = ejecutar_motor_json(st.session_state["jsonValidos"])
-                st.session_state["resultados"] = resultados
+        with st.spinner("Pibble está analizando tus datos, esto puede tardar unos segundos..."):
+            if st.session_state["motor"] == "motorjson":
+                try:
+                    resultados = ejecutar_motor_json(st.session_state["jsonValidos"])
+                    st.session_state["resultados"] = resultados
+                    st.session_state["analisis_listo"] = True
+                    st.rerun()
+                except Exception as e:
+                    st.error(f"Error al analizar los datos: {e}")
+            elif st.session_state["motor"] == "motoroauth":
+                time.sleep(10)
                 st.session_state["analisis_listo"] = True
                 st.rerun()
-            except Exception as e:
-                st.error(f"Error al analizar los datos: {e}")
-        elif st.session_state["motor"] == "motoroauth":
-            time.sleep(10)
-            st.session_state["analisis_listo"] = True
-            st.rerun()
-    
+        
     if st.session_state["analisis_listo"]:
+        contenedorAnimacion.empty()
         st.success("Pibble terminó el análisis")
         if st.button("Ver resultados"):
             if st.session_state["motor"] == "motorjson":
